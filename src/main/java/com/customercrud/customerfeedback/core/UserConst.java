8853a8fa1f56ca7fc8e5d65e80ace17ad6061e17
buddha.stha04 <buddha.stha04@gmail.com>
2020-11-22 15:26:29 +0545

    entity column changed calculate total percentage

--- src/main/java/com/customercrud/customerfeedback/core/UserConst.java
+++ src/main/java/com/customercrud/customerfeedback/core/UserConst.java
@@ -3,6 +3,8 @@ package com.customercrud.customerfeedback.core;
 public class UserConst {
     public static String DEFAULT_USERNAME = "admin";
     public static String DEFAULT_PASSWORD = "123456";
+    public static final double FULL_MARK = 60.00;
+
 
     private UserConst() {
     }
